c1f16c7177dccc7fa530e9c025e99aac5b936350
Dennis Adjei-Baah <adjeibaah@etsu.edu>
2017-10-14 23:00:55 -0700

    additional changes

--- src/main/groovy/com/onecreation/configs/MongoConfiguration.groovy
+++ src/main/groovy/com/onecreation/configs/MongoConfiguration.groovy
@@ -1,18 +1,23 @@
 package com.onecreation.configs
 
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.mongodb.core.MongoClientFactoryBean
 
+import java.util.logging.Logger
+
 @Configuration
 class MongoConfiguration {
+    static Logger log = LoggerFactory.getLogger(MongoConfiguration.class)
 
     @Value('${mongodb.uri}')
     private String mongoUri
 
     @Bean MongoClientFactoryBean mongo() {
-        MongoClientFactoryBean mongo = new MongoClientFactoryBean();
+        MongoClientFactoryBean mongo = new MongoClientFactoryBean()
+        log.info("Current Mongo URI: ${mongoUri}")
         mongo.setHost(mongoUri)
         return mongo
     }
